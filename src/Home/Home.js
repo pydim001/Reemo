f79477bc01d0d227f83e26a3a69a982c8b5e2e73
Aditya Pydimarri <aditya.pydimarri@gmail.com>
2022-07-19 23:09:45 -0500

    Reemo

---
+++ src/Home/Home.js
@@ -0,0 +1,30 @@
+import "./Home.css"
+
+function Home() {
+    return (
+        <div id="app">
+            <div id="when">
+                <div id="date">
+                    <input type="date" id="date-input" />
+                </div>
+                <div id="time">
+                    <input type="time" id="time-input" />
+                </div>
+            </div>
+            <div id="gmail">
+
+            </div>
+            <div id="para">
+
+            </div>
+            <div id="attach">
+
+            </div>
+            <div id="send">
+
+            </div>
+        </div>
+    )
+}
+
+export default Home;
