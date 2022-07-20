1e8ad76f937c4ae48ae03f9dd240dbb122ee83e8
Aditya Pydimarri <aditya.pydimarri@gmail.com>
2022-07-19 23:48:37 -0500

    Reemo UI

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,18 +1,21 @@
-import "./Home.css"
+import "./Home.scss"
 
 function Home() {
     return (
         <div id="app">
             <div id="when">
-                <div id="date">
+                <div id="date" className="when">
+                    <label className="labels">Date:</label>
                     <input type="date" id="date-input" />
                 </div>
-                <div id="time">
+                <div id="time" className="when">
+                    <label className="labels">Time:</label>
                     <input type="time" id="time-input" />
                 </div>
             </div>
             <div id="gmail">
-
+                <label className="labels">Gmail:</label>
+                <input type="text" id="gmail-input" />
             </div>
             <div id="para">
 
