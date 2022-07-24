94aba48227e045226a207112c3f86d13c8622127
Aditya Pydimarri <aditya.pydimarri@gmail.com>
2022-07-23 19:02:57 -0500

    added page states, have to fix attachments

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,25 +1,63 @@
+import { useState } from "react";
 import "./Home.scss"
 
 function Home() {
+
+    const [date, setDate] = useState();
+    const [time, setTime] = useState();
+    const [gmail, setGmail] = useState();
+    const [msg, setMsg] = useState();
+
+    const sDate = (val) => { setDate(val.target.value) }
+    const sTime = (val) => { setTime(val.target.value) }
+    const sGmail = (val) => { setGmail(val.target.value) }
+    const sMsg = (val) => { setMsg(val.target.value) }
+
+    let month = null;
+    let day = null;
+    let year = null;
+
+    let hour = null;
+    let minute = null;
+
+    //formats date
+    const formatTemp = (date) => {
+        const dateArr = date.split("-");
+        const timeArr = time.split(":");
+        [year, month, day] = dateArr;
+        [hour, minute] = timeArr;
+    }
+
+    //button function to send info
+    const send = () => {
+        const info = {
+            date: date,
+            time: time,
+            gmail: gmail,
+            msg: msg
+        }
+        console.log(info);
+    }
+
     return (
         <div id="app">
             <div id="when">
                 <div id="date" className="when">
                     <label className="labels">Date:</label>
-                    <input type="date" id="date-input" />
+                    <input type="date" id="date-input" onChange={sDate} />
                 </div>
                 <div id="time" className="when">
                     <label className="labels">Time:</label>
-                    <input type="time" id="time-input" />
+                    <input type="time" id="time-input" onChange={sTime} />
                 </div>
             </div>
             <div id="gmail">
                 <label className="labels">Gmail:</label>
-                <input type="text" id="gmail-input" />
+                <input type="text" id="gmail-input" onChange={sGmail} />
             </div>
             <div id="para">
                 <label className="labels" id="msg-label">Message:</label>
-                <textarea id="msg-input" rows="10" cols="100"></textarea>
+                <textarea id="msg-input" rows="10" cols="100" onChange={sMsg}></textarea>
             </div>
             <div id="attach">
                 <label for="attack-input" id="custom-attach-input">
@@ -28,9 +66,9 @@ function Home() {
                 </label>
             </div>
             <div id="send">
-                <p id="send-btn">
+                <button id="send-btn" onClick={send}>
                     Send
-                </p>
+                </button>
             </div>
         </div>
     )
